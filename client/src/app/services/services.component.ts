2e81c5ff6df36af70c1779e15a5ee3e1e816c756
ebusovaca <mr.busovaca@gmail.com>
2017-09-22 17:58:30 -0700

    services linked

---
+++ client/src/app/services/services.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api/api.service';
+import { LoginService } from '../util/login.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../api/User';
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './services.component.html',
+  styleUrls: ['./services.component.css']
+})
+export class ServicesComponent implements OnInit {
+  public user: User;
+
+  constructor(public loginService: LoginService, private api: ApiService, private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+    this.api.getUser(this.loginService.userId)
+      .then(user => this.user = user)
+      .catch(error => {
+        console.log('Error getting user profile');
+        console.log(error);
+        // TODO Handle this case, redirect to the home page maybe.
+      });
+  }
+
+
+}
